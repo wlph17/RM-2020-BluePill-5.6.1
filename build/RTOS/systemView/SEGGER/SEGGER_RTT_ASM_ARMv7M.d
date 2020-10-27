bc71d664822204454b9f7fc7367c75bc2e071a18
wlph17 <wlph17@gmail.com>
2020-10-27 12:45:05 +0800

    init

--- build/RTOS/systemView/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,8 +0,0 @@
-build/RTOS/systemView/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
-  RTOS/systemView/SEGGER/SEGGER_RTT_ASM_ARMv7M.s \
-  RTOS/systemView/SEGGER/SEGGER_RTT.h \
-  RTOS/systemView/Config/SEGGER_RTT_Conf.h
-
-RTOS/systemView/SEGGER/SEGGER_RTT.h:
-
-RTOS/systemView/Config/SEGGER_RTT_Conf.h:
